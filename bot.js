cc7cacc711b00f69032108b04fcd6b8df42a87dd
Kouhei Nakata <kouhei.6835@gmail.com>
2024-01-27 20:22:49 +0900

    Your commit message

--- bot.js
+++ bot.js
@@ -4,7 +4,7 @@ const config = require('./config.json');
 const stripe = require('stripe')(config.stripe.secret_key);
 
 const client = new Discord.Client({
-  intents: [Discord.Intents.FLAGS.Guilds, Discord.Intents.FLAGS.MessageContent, Discord.Intents.FLAGS.DirectMessageContent]
+  intents: [Discord.Intents.FLAGS.Guilds]
 });
 
 client.commands = new Discord.Collection();
